cfc59cec06f1c113fd425ab4f59669ba3138b7dd
Stinson <stinsonzhao@gmail.com>
2021-07-06 15:58:56 +0800

    feat(header&footer): render header and footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Double-click to edit a todo</p>
+      <p>
+        Refer to <a href="http://todomvc.com">TodoMVC</a>
+      </p>
+    </footer>
+  )
+}
